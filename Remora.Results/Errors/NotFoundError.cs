29d2608583a999123d33c906c2359fce24d9a2fe
Jarl Gullberg <jarl.gullberg@gmail.com>
2021-08-18 19:00:46 +0200

    Add a generalized error for when something isn't found.

---
+++ Remora.Results/Errors/NotFoundError.cs
@@ -0,0 +1,30 @@
+//
+//  NotFoundError.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+namespace Remora.Results
+{
+    /// <summary>
+    /// Represents a failure to find something that was searched for.
+    /// </summary>
+    /// <param name="Message">The custom message to provide.</param>
+    public record NotFoundError(string Message = "The searched-for entity was not found.") : ResultError(Message);
+}
