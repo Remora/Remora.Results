b1a4d6269dfd9658db7bad3ef196b7f0e6a03dca
Jarl Gullberg <jarl.gullberg@gmail.com>
2021-09-04 13:25:29 +0200

    Add an aggregate error.

---
+++ Remora.Results/Errors/AggregateError.cs
@@ -0,0 +1,59 @@
+//
+//  AggregateError.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+
+namespace Remora.Results
+{
+    /// <summary>
+    /// Represents a set of errors produced by an operation.
+    /// </summary>
+    /// <param name="Errors">The errors.</param>
+    /// <param name="Message">The custom error message, if any.</param>
+    /// <remarks>Used in place of <see cref="AggregateException"/>.</remarks>
+    public record AggregateError
+    (
+        IReadOnlyCollection<IResultError> Errors,
+        string Message = "One or more errors occurred."
+    ) : ResultError(Message)
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateError"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="errors">The errors.</param>
+        public AggregateError(string message, params IResultError[] errors)
+            : this(errors, message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateError"/> class.
+        /// </summary>
+        /// <param name="errors">The errors.</param>
+        public AggregateError(params IResultError[] errors)
+            : this((IReadOnlyCollection<IResultError>)errors)
+        {
+        }
+    }
+}
