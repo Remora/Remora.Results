92134b1ed93c29b248b453f75e07191eb7259894
Foxtrek_64 <foxtrek64@users.noreply.github.com>
2024-06-04 15:16:22 -0500

    Initial implementation of AggregateResult and AggregateResultBuilder.

---
+++ Remora.Results/AggregateResult.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Remora.Results;
+
+/// <inheritdoc />
+[PublicAPI]
+public readonly struct AggregateResult : IResult
+{
+    /// <summary>
+    /// Gets a value indicating whether all the contained results were successful.
+    /// </summary>
+    public bool IsSuccess { get; }
+
+    /// <summary>
+    /// Gets an <see cref="AggregateError"/> containing the failed results.
+    /// </summary>
+    public IResultError? Error => new AggregateError(_lookup[false].ToArray());
+
+    /// <inheritdoc />
+    public IResult? Inner { get; } = null;
+
+    /// <summary>
+    /// Gets a collection of successful results.
+    /// </summary>
+    public IEnumerable<IResult> SuccessfulResults => _lookup[true];
+
+    /// <summary>
+    /// Gets a collection of failed results.
+    /// </summary>
+    public IEnumerable<IResult> FailedResults => _lookup[false];
+
+    /// <summary>
+    /// Gets a readonly collection of the results contained in this collection.
+    /// </summary>
+    public IReadOnlyCollection<IResult> Results => _results;
+
+    private readonly IResult[] _results;
+    private readonly ILookup<bool, IResult> _lookup;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AggregateResult"/> struct.
+    /// </summary>
+    /// <param name="results">The results to use.</param>
+    public AggregateResult(params IResult[] results)
+        : this(results.All(it => it.IsSuccess), results)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AggregateResult"/> struct.
+    /// </summary>
+    /// <param name="isSuccess">A value indicating whether all the results are successful.</param>
+    /// <param name="results">The results.</param>
+    internal AggregateResult(bool isSuccess, IResult[] results)
+    {
+        this.IsSuccess = isSuccess;
+        _results = results;
+        _lookup = this.Results.ToLookup(it => it.IsSuccess);
+    }
+}
---
+++ Remora.Results/AggregateResultBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Remora.Results;
+
+/// <summary>
+/// Allows for the construction of an AggregateResult.
+/// </summary>
+/// <remarks>
+/// This is intended for use within a foreach loop or in situations where results are added on their own
+/// rather than as a group. If you have all of the results available together, use <see cref="AggregateResult.New()"/>
+/// </remarks>
+[PublicAPI]
+public class AggregateResultBuilder
+{
+    private List<IResult> _results = new();
+
+    /// <summary>
+    /// Adds the result to the collection.
+    /// </summary>
+    /// <param name="result">The result to add.</param>
+    public void Add(IResult result) => _results.Add(result);
+
+    /// <summary>
+    /// Builds the <see cref="AggregateResult"/>.
+    /// </summary>
+    /// <returns>A new AggregateResult.</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public AggregateResult Build()
+    {
+        throw new NotImplementedException();
+    }
+}
